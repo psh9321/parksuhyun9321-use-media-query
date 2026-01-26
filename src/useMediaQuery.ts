79a3dcbb009755c484c1e374b4dab936be3de29f
psh9321 <parksuhyun9321@gmail.com>
2026-01-26 13:37:31 +0900

    초기 반영

---
+++ src/useMediaQuery.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react"
+
+export const useMediaQuery = (maxWidth : number) => {
+
+    const [ isResize, SetIsResize ] = useState<boolean>(() => {
+        if(typeof window === "undefined") return true
+        return window.innerWidth < maxWidth
+    });
+
+    function ResizeCallback() {
+        const value = window.innerWidth < maxWidth;
+        SetIsResize(prev => prev === value ? prev : value);
+    }
+
+    useEffect(() => {
+        window.addEventListener("resize", ResizeCallback)
+
+        return () => {
+            window.removeEventListener("resize", ResizeCallback)
+        }
+    }, [maxWidth])
+
+    return {isResize}
+}
